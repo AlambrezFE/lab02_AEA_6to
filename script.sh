2128e67c3405597e38589257c21ff1ef81dbadf5
AlambrezFE <andre29flores.aga.nst@gmail.com>
2024-08-29 05:07:44 -0500

    errores menores

--- script.sh
+++ script.sh
@@ -27,7 +27,7 @@ container_id=$(docker ps -qf "name=gifapi")
 container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)
 
 # Exportar la IP como una variable de entorno
-export API_URL="http://$container_ip:80/api/"
+export API_URL="http://$container_ip:80/api"
 
 # Ejecutar el contenedor del frontend con la variable de entorno
 docker run -d -p 5001:80 -e API_URL=$API_URL giffrontend
